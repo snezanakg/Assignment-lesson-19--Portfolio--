1ec49b718e2ca8f581ae59e4ccfa0371c1663085
snezanakg <142974537+snezanakg@users.noreply.github.com>
2024-01-07 17:47:22 +0100

    script.js writen

---
+++ script.js
@@ -0,0 +1,34 @@
+//function to toggle the mobile navigation menu
+function toggleMenu() {
+    const navLinks=
+    document.querySelector('.nav-links');
+    const burger=
+    document.querySelector('.burger');
+     navLinks.classList.toggle('active');
+}
+//event listener for the mobile navigation menu
+document.querySelector('.burger').addEventListener('click', toggleMenu);
+//function to handle form submission
+function submitForm() {
+   function submitForm(event) {
+         event.preventDefault();
+    //get the values from the form
+    const name=
+    document.getElementById('name').value;
+    const email=
+    document.getElementById('email').value;
+    const message=
+    document.getElementById('message').value;
+
+    //send formdate to beckend or perform desired action
+    console.log("Name: "+name);
+    console.log("Email: "+email);
+    console.log("Message: "+message);
+}
+//reset the form
+document.getElementById("contact-form").reset();
+
+//event listener for the form submission
+document.getElementById('contact-form').addEventListener('submit', submitForm);
+
+   
